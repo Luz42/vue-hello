40ba0e2e23a52fdedb5885c30a673e1aea7e77cc
Luz42 <a.evangelista93@gmail.com>
2022-09-12 21:27:40 +0200

    Viene importato lo script nell'HEAD
    è stata chiamata la funzione/app di VUE in JS
    
    viene individuato il container dell'app nell'HTML
    
    due input prendono dei valori che cambiano stile e testo all'elemento
    
    successivamente è stata creata una presentazione dinamica

---
+++ myScript.js
@@ -0,0 +1,16 @@
+console.log('JS-OK');
+
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+
+            saluto: 'Hello Vue!',
+            spostaIlSaluto: 'text-center py-3 bg-dark text-white',
+            userName: '',
+            userImage: '',
+            userPresentation:''
+
+        }
+    }
+)
